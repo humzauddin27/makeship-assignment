8ccd5b7c31fd08cd17cbce0d605a0678eceea629
Humza Faheemuddin <humzauddin7@gmail.com>
2020-08-07 03:39:51 -0400

    updating const to include heroku

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:8080";
+  process.env.NODE_ENV === "production"
+    ? "https://humzas-image-viewer-backend.herokuapp.com/"
+    : "http://localhost:8080";
